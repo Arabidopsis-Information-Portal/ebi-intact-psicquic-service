dddc1cb9e95a25d6a08bceffb37f22e1525c516a
eriksf <erik@jcvi.org>
2015-07-16 15:21:03 -0400

    now parsing the individual data fields

--- services/common/tools.py
+++ services/common/tools.py
@@ -4,6 +4,137 @@ import urlparse
 import json
 
 EBI_INTACT_BASE_URL = 'http://www.ebi.ac.uk/Tools/webservices/psicquic/intact/webservices/current/search/query/'
+DATABASE_URLS = {'uniprotkb': 'http://www.uniprot.org/uniprot/',
+                 'intact': 'http://www.ebi.ac.uk/intact/',
+                 'taxid': 'http://www.uniprot.org/taxonomy/',
+                 'psi-mi': 'http://www.ebi.ac.uk/ontology-lookup/browse.do?ontName=MI&termId=',
+                 'pubmed': 'http://www.ncbi.nlm.nih.gov/pubmed/'}
+
+class MITabTextDecoder:
+    def __init__(self, text):
+        self.buffer = text if text else ""
+        self.offset = 0
+
+    def nextToken(self, delimiter):
+        token = ''
+        if (self.offset < len(self.buffer)):
+            start = self.offset
+            index = self.buffer.find(delimiter, start)
+            if (index != -1):
+                self.offset = index
+                token = self.buffer[start:index]
+                self.offset += len(delimiter)
+            else:
+                self.offset = len(self.buffer)
+                token = self.buffer[start:]
+        return token
+
+    def hasNext(self):
+        return self.offset < len(self.buffer)
+
+    def decodeXref(self):
+        xref = {'key':'', 'value':'', 'desc':''}
+        token = self.nextToken(':')
+        if (len(token) > 1):
+            xref['key'] = token
+            xref['value'] = self.nextToken('|').replace('"','')
+            descIndex = xref['value'].find('(')
+            if (descIndex != -1):
+                xref['desc'] = xref['value'][descIndex+1:len(xref['value'])-1]
+                xref['value'] = xref['value'][0:descIndex]
+        return xref
+
+def getProteinXref(text):
+    protein_xref = {'id': '', 'url': ''}
+    ids = []
+    urls = []
+    decoder = MITabTextDecoder(text)
+    while decoder.hasNext():
+        xref = decoder.decodeXref()
+        ids.append(xref['value'])
+        if DATABASE_URLS.has_key(xref['key']):
+            base_url = DATABASE_URLS[xref['key']]
+            urls.append("%s%s" % (base_url, xref['value']))
+        else:
+            urls.append(xref['key']+":"+xref['value'])
+    if (len(ids) > 0):
+        protein_xref['id'] = ", ".join(ids)
+        protein_xref['url'] = ", ".join(urls)
+    return protein_xref
+
+def getInteractionXref(text):
+    interaction_xref = {'id': '', 'url': ''}
+    ids = []
+    urls = []
+    decoder = MITabTextDecoder(text)
+    while decoder.hasNext():
+        xref = decoder.decodeXref()
+        ids.append(xref['value'])
+        if DATABASE_URLS.has_key(xref['key']):
+            base_url = DATABASE_URLS[xref['key']]
+            urls.append("%sinteraction/%s" % (base_url, xref['value']))
+        else:
+            urls.append(xref['key']+":"+xref['value'])
+    if (len(ids) > 0):
+        interaction_xref['id'] = ", ".join(ids)
+        interaction_xref['url'] = ", ".join(urls)
+    return interaction_xref
+
+def getValueXref(text):
+    value_xref = {'id': '', 'url': ''}
+    ids = []
+    urls = []
+    decoder = MITabTextDecoder(text)
+    while decoder.hasNext():
+        xref = decoder.decodeXref()
+        val = xref['key'] + ':' + xref['value']
+        ids.append(val)
+        if DATABASE_URLS.has_key(xref['key']):
+            base_url = DATABASE_URLS[xref['key']]
+            urls.append("%s%s" % (base_url, xref['value']))
+        else:
+            urls.append(val)
+    if (len(ids) > 0):
+        value_xref['id'] = ", ".join(ids)
+        value_xref['url'] = ", ".join(urls)
+    return value_xref
+
+def getValue(text):
+    value = ''
+    values = []
+    decoder = MITabTextDecoder(text)
+    while decoder.hasNext():
+        xref = decoder.decodeXref()
+        values.append(xref['value'])
+    if (len(values) > 0):
+        value = ", ".join(values)
+    return value
+
+def getRawValue(text):
+    value = ''
+    decoder = MITabTextDecoder(text)
+    if decoder.hasNext():
+        xref = decoder.decodeXref()
+        value = xref['value']
+    return value
+
+def getDescriptionValueXref(text):
+    desc_xref = {'id': '', 'url': ''}
+    ids = []
+    urls = []
+    decoder = MITabTextDecoder(text)
+    while decoder.hasNext():
+        xref = decoder.decodeXref()
+        ids.append(xref['desc'])
+        if DATABASE_URLS.has_key(xref['key']):
+            base_url = DATABASE_URLS[xref['key']]
+            urls.append("%s%s" % (base_url, xref['value']))
+        else:
+            urls.append(xref['key']+":"+xref['value'])
+    if (len(ids) > 0):
+        desc_xref['id'] = ", ".join(ids)
+        desc_xref['url'] = ", ".join(urls)
+    return desc_xref
 
 def is_valid_agi_identifier(ident):
     p = re.compile(r'AT[1-5MC]G[0-9]{5,5}\.[0-9]+', re.IGNORECASE)
--- services/ebi_intact_by_locus/main.py
+++ services/ebi_intact_by_locus/main.py
@@ -23,26 +23,44 @@ def search(args):
     """
     for result in response.iter_lines():
         fields = result.strip().split('\t')
+        ident_a = tools.getProteinXref(fields[0])
+        ident_b = tools.getProteinXref(fields[1])
+        int_detect_methods = tools.getDescriptionValueXref(fields[6])
+        pub_ids = tools.getValueXref(fields[8])
+        ncbi_tax_a = tools.getDescriptionValueXref(fields[9])
+        ncbi_tax_b = tools.getDescriptionValueXref(fields[10])
+        int_types = tools.getDescriptionValueXref(fields[11])
+        source_db = tools.getDescriptionValueXref(fields[12])
+        int_id_source = tools.getInteractionXref(fields[13])
         record = {
                 'locus': locus,
                 'class': 'locus_property',
                 'source_text_description': 'Molecular Interactions',
                 'interaction_record': {
-                    'unique_identifier_for_interactor_a': fields[0],
-                    'unique_identifier_for_interactor_b': fields[1],
-                    'alt_identifier_for_interactor_a': fields[2],
-                    'alt_identifier_for_interactor_b': fields[3],
-                    'aliases_for_a': fields[4],
-                    'aliases_for_b': fields[5],
-                    'interaction_detection_methods': fields[6],
+                    'unique_identifier_for_interactor_a': ident_a['id'],
+                    'unique_identifier_for_interactor_a_url': ident_a['url'],
+                    'unique_identifier_for_interactor_b': ident_b['id'],
+                    'unique_identifier_for_interactor_b_url': ident_b['url'],
+                    'alt_identifier_for_interactor_a': tools.getValue(fields[2]),
+                    'alt_identifier_for_interactor_b': tools.getValue(fields[3]),
+                    'aliases_for_a': tools.getValue(fields[4]),
+                    'aliases_for_b': tools.getValue(fields[5]),
+                    'interaction_detection_methods': int_detect_methods['id'],
+                    'interaction_detection_methods_url': int_detect_methods['url'],
                     'first_author': fields[7],
-                    'publication_identifier': fields[8],
-                    'ncbi_tax_identifier_for_interactor_a': fields[9],
-                    'ncbi_tax_identifier_for_interactor_b': fields[10],
-                    'interaction_types': fields[11],
-                    'source_databases': fields[12],
-                    'interaction_identifiers_in_source': fields[13],
-                    'confidence_score': fields[14]
+                    'publication_identifier': pub_ids['id'],
+                    'publication_identifier_url': pub_ids['url'],
+                    'ncbi_tax_identifier_for_interactor_a': ncbi_tax_a['id'],
+                    'ncbi_tax_identifier_for_interactor_a_url': ncbi_tax_a['url'],
+                    'ncbi_tax_identifier_for_interactor_b': ncbi_tax_b['id'],
+                    'ncbi_tax_identifier_for_interactor_b_url': ncbi_tax_b['url'],
+                    'interaction_types': int_types['id'],
+                    'interaction_types_url': int_types['url'],
+                    'source_databases': source_db['id'],
+                    'source_databases_url': source_db['url'],
+                    'interaction_identifiers_in_source': int_id_source['id'],
+                    'interaction_identifiers_in_source_url': int_id_source['url'],
+                    'confidence_score': tools.getRawValue(fields[14])
                 }
             }
         print json.dumps(record, indent=2)
